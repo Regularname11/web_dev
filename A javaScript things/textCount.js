03eaeb7fa9adbcf6556a11b60fe047b722e60c0a
Taha Enn <78084972+Regularname11@users.noreply.github.com>
2024-06-22 19:02:21 +0300

    adding if statment for more 150 characters

--- A javaScript things/textCount.js
+++ A javaScript things/textCount.js
@@ -1,4 +1,10 @@
-// Text counter. limit is 150 character.
-var text = prompt ("Hi, enter your text here: 👇");
-var textCount = text.length;
-alert ("you have written " + textCount + " character, and you still have " + (150 - textCount) + " character left." )
+// Text counter with a limit of 150 characters.
+var text = prompt("Hi, enter your text here: 👇");
+
+if (text.length > 150) {
+    alert("You have reached your limit which is 150 characters.");
+}
+else {
+    var textCount = text.length;
+    alert("You have written " + textCount + " characters, and you still have " + (150 - textCount) + " characters left.");
+}
