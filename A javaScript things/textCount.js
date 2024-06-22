bfabf1c78bdfa6d57f439a207b8276ffbe72bd2d
Taha Enn <78084972+Regularname11@users.noreply.github.com>
2024-06-22 18:03:18 +0300

    Creating a Text counter with a limit of 150 character

---
+++ A javaScript things/textCount.js
@@ -0,0 +1,4 @@
+// Text counter. limit is 150 character.
+var text = prompt ("Hi, enter your text here: 👇");
+var textCount = text.length;
+alert ("you have written " + textCount + " character, and you still have " + (150 - textCount) + " character left." )
