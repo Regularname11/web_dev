f0190e4821674f6bc205169e7af968322d2e89ac
Taha Enn <78084972+Regularname11@users.noreply.github.com>
2024-06-23 21:11:43 +0300

    working with lower and uppercase (slicing, extracting and string casing).

--- a_javaScript_things/newfile1.js
+++ a_javaScript_things/newfile1.js
@@ -1,4 +1,4 @@
-//working with lower and uppercase (slicing, extracting and string casing)
+//working with lower and uppercase (slicing, extracting and string casing).
 
 var name = prompt("What is your name?");
 alert("Hello, " + name.slice(0,1).toUpperCase() + name.slice(1,name.length).toLowerCase());
