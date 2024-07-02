1670cd769fafa650ac57c32c77148c34157d91a7
Taha Enn <78084972+Regularname11@users.noreply.github.com>
2024-07-02 18:31:40 +0300

    Fixing prompt message

--- a_javaScript_things/catAge.js
+++ a_javaScript_things/catAge.js
@@ -1,11 +1,11 @@
-// Your cat age according to human age.
-
-function catAge (humanAge){
+function catAge (){
     var catAge = 0;
-    prompt ("Enter your cqt age:")
-    if (humanAge === 1) return 15;
-    if (humanAge === 2) return 25;
+    var humanAge = parseInt(prompt ("Enter your cat age:"));
+    if (humanAge === 1) return alert ("Your cat age is 15 years old in human age.");
+    if (humanAge === 2) return alert ("Your cat age is 25 years old in human age.");
 
     catAge = (humanAge-2) * 4 + 25;
+
     return alert ("Your cat age is " + catAge + " years old in human age.");
 }
+catAge();
